d3e711b77a63bdfe650ba77087ebfd03b0f05e36
rdchen1124 <rdchen1124@gmail.com>
2021-08-20 17:19:26 +0800

    modify DUMMY_STORES.js

--- src/DUMMY_STORES.js
+++ src/DUMMY_STORES.js
@@ -1,8 +1,27 @@
 const DUMMY_STORES = [
   {
-    id: 's1',
+    id: 's0001',
+    menu_id: 'menu_s0001',
     name: '麥當勞-西屯店',
     score: 4.7,
-    
+    type: 'DINER',
+    favorite: false
+  },
+  {
+    id: 's0002',
+    menu_id: 'menu_s0002',
+    name: '可不可熟成紅茶',
+    score: 4.1,
+    type: 'DRINK_SHOP',
+    favorite: false
+  },
+  {
+    id: 's0003',
+    menu_id: 'menu_s0003',
+    name: '家樂福-東海店',
+    score: 4.3,
+    type: 'MERKET',
+    favorite: false
   }
 ]
+export default DUMMY_STORES;
