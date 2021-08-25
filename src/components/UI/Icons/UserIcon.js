3789bd46d5a21eedece0f82a7cd2a1699cfdb9c7
rdchen1124 <rdchen1124@gmail.com>
2021-08-25 16:28:34 +0800

    Add UserIcon include svg for UserButton.

---
+++ src/components/UI/Icons/UserIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserIcon = () => {
+  return (
+    <svg 
+      aria-hidden="true"
+      focusable="false"
+      viewBox="0 0 20 20"
+      class="c6 c7 c8 c9"
+    >
+      <path d="M19.167 3.333H.833v2.5h18.334v-2.5zm0 5.834H.833v2.5h18.334v-2.5zM.833 15h18.334v2.5H.833V15z" />
+    </svg>
+  )
+}
+export default UserIcon;
---
+++ src/components/UI/Icons/index.js
@@ -0,0 +1,3 @@
+import UserIcon from "./UserIcon";
+import CartIcon from "./CartIcon";
+export { UserIcon, CartIcon }
