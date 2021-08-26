88db6e685bec13aead94e47033a36b06ddc82401
rdchen1124 <rdchen1124@gmail.com>
2021-08-26 12:32:14 +0800

    Add style to Cart, and add handleClose test code to it also.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,27 @@
+import { CartModal } from "../UI";
+import styled from "styled-components"; 
+const CloseButton = styled.div`
+  border-radius: 5px;
+  background: #f44336;
+  color: white;
+  height: 30px;
+  width: 80px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 const Cart = (props) => {
+  const handleClose = () => {
+    console.log('Cart Close!!');
+  }
   return (
-    <div>
+    <CartModal>
+      <CloseButton onClick={handleClose}>
+        Close
+      </CloseButton>
       CartModalContent
-    </div>
+    </CartModal>
   )
 }
 export default Cart;
