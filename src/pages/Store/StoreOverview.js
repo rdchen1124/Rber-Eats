54b205919a0859c93b75052be45920cc617f363e
rdchen1124 <rdchen1124@gmail.com>
2021-08-28 15:47:47 +0800

    Modify style of StoreOverview.

--- src/pages/Store/StoreOverview.js
+++ src/pages/Store/StoreOverview.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 const StoreOverviewWrapper = styled.div`
-  width: 100%;
+  width: 90%;
   height: 200px;
   margin: 0 auto;
   padding: 25px 10px;
@@ -9,21 +9,23 @@ const StoreOverviewWrapper = styled.div`
 const StoreOverviewContainer = styled.div`
   width: 100%;
   height: 100%;
+  margin: 0 auto;
   background: rgba(0, 0, 0, 0.3);
   border: none;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: start;
-  padding-left: 20px;
 `
 const StoreName = styled.div`
+  padding-left: 20px;
   color: black;
   font-size: 42px;
   font-weight: bold;
 `
 const StoreInfo = styled.div`
+  padding-left: 20px;
   color: white;
   font-size: 28px;
 `
