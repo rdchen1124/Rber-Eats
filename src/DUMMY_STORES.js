a6fd016d1151e0373e57c3459fe5100541fd5f48
rdchen1124 <rdchen1124@gmail.com>
2021-08-28 12:45:57 +0800

    Modify Dummy_Store with demo.

--- src/DUMMY_STORES.js
+++ src/DUMMY_STORES.js
@@ -1,6 +1,7 @@
 const DUMMY_STORES = [
   {
     id: 's0001',
+    image: 'McDonald',
     menu_id: 'menu_s0001',
     name: '麥當勞-西屯店',
     score: 4.7,
@@ -9,6 +10,7 @@ const DUMMY_STORES = [
   },
   {
     id: 's0002',
+    image: 'KEBUKE',
     menu_id: 'menu_s0002',
     name: '可不可熟成紅茶',
     score: 4.1,
@@ -17,11 +19,40 @@ const DUMMY_STORES = [
   },
   {
     id: 's0003',
+    image: 'Carrefour',
     menu_id: 'menu_s0003',
     name: '家樂福-東海店',
     score: 4.3,
     type: 'MERKET',
     favorite: false
+  },
+  {
+    id: 's0004',
+    image: 'Subway',
+    menu_id: 'menu_s0004',
+    name: '賽百味潛艇堡',
+    score: 4.2,
+    type: 'DINER',
+    favorite: false
+  },
+  {
+    id: 's0005',
+    image: 'WOOHOLD',
+    menu_id: 'menu_s0005',
+    name: '武厚茶-恆春夜市總店',
+    score: 5.0,
+    type: 'DRINK_SHOP',
+    favorite: false
+  },
+  {
+    id: 's0006',
+    image: 'PX_Mart',
+    menu_id: 'menu_s0006',
+    name: '全聯福利中心-中科店',
+    score: 3.9,
+    type: 'MERKET',
+    favorite: false
   }
+
 ]
 export default DUMMY_STORES;
