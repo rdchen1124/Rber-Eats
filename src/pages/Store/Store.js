a21f1a7319d079c00adfb62534ae287ffc2a74dc
rdchen1124 <rdchen1124@gmail.com>
2021-08-28 16:07:26 +0800

    Add useEffect to set store state by id whenever id is changed.

--- src/pages/Store/Store.js
+++ src/pages/Store/Store.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useParams } from 'react-router';
 import Root from '../../components/Root';
 import Stores from '../../components/Stores';
 import StoreOverview from './StoreOverview';
-
+import DUMMY_STORES from '../../DUMMY_STORES';
 const Store = () => {
   let { id } = useParams();
+  const [store, setStore] = useState({});
+  useEffect(()=>{
+    setStore(DUMMY_STORES.filter(shop => id === shop.id)[0])
+  }, [id])
   return (
     <Root>
-      <StoreOverview id={id} />
+      { store && 
+        <StoreOverview id={id} name={store.name} score={store.score} />}
       <main>
         <Stores />
       </main>
