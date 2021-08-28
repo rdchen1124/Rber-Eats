7f1996f0c2fd6905a7ca28bd069e7c1f5f6ccd31
rdchen1124 <rdchen1124@gmail.com>
2021-08-28 15:38:00 +0800

    Modify style of StoreOverview and add name, score to it.

--- src/pages/Store/StoreOverview.js
+++ src/pages/Store/StoreOverview.js
@@ -1,5 +1,40 @@
 import React from 'react'
+import styled from 'styled-components';
+const StoreOverviewWrapper = styled.div`
+  width: 100%;
+  height: 200px;
+  margin: 0 auto;
+  padding: 25px 10px;
+`
+const StoreOverviewContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+  border: none;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  padding-left: 20px;
+`
+const StoreName = styled.div`
+  color: black;
+  font-size: 42px;
+  font-weight: bold;
+`
+const StoreInfo = styled.div`
+  color: white;
+  font-size: 28px;
+`
 const StoreOverview = ({id}) => {
-  return (<div>Store_{id}</div>)
+  return (
+    <StoreOverviewWrapper>
+      <StoreOverviewContainer>
+        <StoreName>Store_{id}</StoreName>
+        <StoreInfo>評分: 4.7</StoreInfo>
+      </StoreOverviewContainer>
+    </StoreOverviewWrapper>
+  )
 }
 export default StoreOverview
