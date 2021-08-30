2beabb06f63e6715c66eb4fd3252870d8232e9b3
rdchen1124 <rdchen1124@gmail.com>
2021-08-30 18:19:34 +0800

    Add another row of MealItem of  AvailableMeals.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -10,6 +10,9 @@ const AvailableMeals = (props) => {
         <MealItem id="m0004" name="meal_0004" img="img_0004" price="40" description="test_4" />
         <MealItem id="m0005" name="meal_0005" img="img_0005" price="50" description="test_5" />
         <MealItem id="m0006" name="meal_0006" img="img_0006" price="60" description="test_6" />
+        <MealItem id="m0007" name="meal_0007" img="img_0007" price="70" description="test_7" />
+        <MealItem id="m0008" name="meal_0008" img="img_0008" price="80" description="test_8" />
+        <MealItem id="m0009" name="meal_0009" img="img_0009" price="90" description="test_9" />
       </MealsCard>
     </session>
   )
