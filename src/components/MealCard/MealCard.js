79b205dd59f491ca3beda5289c03f63d6825efda
rdchen1124 <rdchen1124@gmail.com>
2021-08-31 18:27:24 +0800

    Add Close component to MealCard which at left-top absolutely.

--- src/components/MealCard/MealCard.js
+++ src/components/MealCard/MealCard.js
@@ -5,11 +5,34 @@ import { MealModal } from "../UI";
 import { hideMenu } from "../../redux/reducers/cartReducer";
 
 const MealContainer = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
 `;
+const CloseButton = styled.div`
+  position: absolute;
+  cursor: pointer;
+  left: 5px;
+  top: 5px;
+  background: white;
+  width: 40px;
+  height: 40px;
+  border: none;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    background: grey;
+  }
+`
+const Close = ({onClose}) => {
+  return( 
+    <CloseButton onClick={onClose}>X</CloseButton>
+  )
+}
 const ImageContainer = styled.div`
   width: 100%;
   height: 40%;
@@ -17,6 +40,9 @@ const ImageContainer = styled.div`
   color: white;
   font-size: 28px;
   font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const MealInfoContainer = styled.div`
   width: 100%;
@@ -49,6 +75,7 @@ const MealCard = () => {
   return (
     <MealModal onClose={handleClose}>
       <MealContainer>
+        <Close onClose={handleClose} />
         <ImageContainer>{meal.img}</ImageContainer>
         <MealInfoContainer>
           <MealName>{meal.name}</MealName>
