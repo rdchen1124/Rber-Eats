f18cb22518f09352b9c8b42a054353bedf276619
rdchen1124 <rdchen1124@gmail.com>
2021-08-31 16:53:08 +0800

    Add menuReducer to store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './reducers/userReducer'
 import cartReducer from './reducers/cartReducer'
+import menuReducer from './reducers/menuReducer'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    menu: menuReducer
   },
 })
