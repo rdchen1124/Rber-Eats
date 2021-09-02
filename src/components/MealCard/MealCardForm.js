f963683f7eeb5cd42fbac7b07d131b45fb3b605c
rdchen1124 <rdchen1124@gmail.com>
2021-09-02 15:39:22 +0800

    Add amountInput state and pass it to AddToCartButton to showing current amount of meal.

--- src/components/MealCard/MealCardForm.js
+++ src/components/MealCard/MealCardForm.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState } from "react";
 const MealFormWrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -6,13 +7,19 @@ const MealFormWrapper = styled.div`
 `
 const FormControl = styled.div``;
 const SubmitButton = styled.button`
-  width: 200px;
+  width: 100%;
   background: black;
   color: white;
   border: none;
   border-radius: 3px;
   height: 30px;
+  box-sizing: border-box;
 `
+const AddToCartButton = ({amount, price})=>{
+  return (
+    <SubmitButton>新增 {amount} 項商品至購物車 NT.{amount*price}</SubmitButton>
+  )
+}
 const FormInput = styled.input`
   height: 30px;
   padding: 5px 10px;
@@ -24,17 +31,36 @@ const inputSetting = {
   min: '1',
   max: '10',
   step: '1',
-  defaultValue: '1',
 }
-const MealCardForm = ({onSubmit}) => {
+const ControlledInput = ({value, onChange, inputSetting})=>{
   return (
-    <form onSubmit={onSubmit}>
+    <FormInput value={value} onChange={onChange} {...inputSetting} />
+  )
+}
+const MealCardForm = ({price, onAddToCart}) => {
+  const [amountInput, setAmountInput] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddToCart(amountInput);
+  }
+
+  const handleAmountChange = (e) => {
+    setAmountInput(e.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
       <MealFormWrapper>
         <FormControl>
-          <FormInput {...inputSetting} />
+          <ControlledInput
+            value={amountInput}
+            onChange={handleAmountChange}
+            inputSetting={inputSetting}
+          />
         </FormControl>
         <FormControl>
-          <SubmitButton>新增至購物車</SubmitButton>
+          <AddToCartButton amount={amountInput} price={price} />
         </FormControl>
       </MealFormWrapper>
     </form>
