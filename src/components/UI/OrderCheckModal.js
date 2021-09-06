2d5217b40c844564d527480e3ef912b049008739
rdchen1124 <rdchen1124@gmail.com>
2021-09-06 16:00:53 +0800

    Add OrderCheckModal which is showing when adding items form different store.

---
+++ src/components/UI/OrderCheckModal.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+const OrderCheckModalWrapper = styled.div`
+  background: white;
+  position: fixed;
+  width: auto;
+  top: 10%;
+  left: 30%;
+  right: 30%;
+  bottom: 50%;
+  z-index: 40;
+  padding: 15px 20px;
+`;
+const OrderCheckModal = (props) => {
+  return (
+    <OrderCheckModalWrapper>
+      <div>是否建立新訂單?</div>
+      <div>您的購物車已有來自不同商店的商品，請先清空或結帳</div>
+      <div>
+        <button onClick={props.onClose}>取消</button>
+        <button>新訂單</button>
+      </div>
+    </OrderCheckModalWrapper>
+  )
+}
+export default OrderCheckModal;
