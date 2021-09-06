a4683f353d98468c90e015a21b8ac241afdb1811
rdchen1124 <rdchen1124@gmail.com>
2021-09-06 15:49:29 +0800

    Add isOrderChecking state to decide to show OrderCheckModal or not.

--- src/redux/reducers/menuReducer.js
+++ src/redux/reducers/menuReducer.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   isMenuShowing: false,
+  isOrderChecking: false,
   meal: {}
 }
 
@@ -18,10 +19,16 @@ export const menuSlice = createSlice({
     hideMenu: (state) => {
       state.isMenuShowing = false;
     },
+    showCheckModal: (state) => {
+      state.isOrderChecking = true;
+    },
+    hideCheckModal: (state) => {
+      state.isOrderChecking = false;
+    },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { setMeal, showMenu, hideMenu } = menuSlice.actions
+export const { setMeal, showMenu, hideMenu, showCheckModal, hideCheckModal } = menuSlice.actions
 
 export default menuSlice.reducer
