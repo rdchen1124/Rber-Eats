eb1b7c26aeb62bc81422ee542bcd0e03d7f76402
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-09-28 17:54:34 +0800

    Let UserCard shows 2 kind of content by state.user.

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,6 +1,25 @@
 import React, {useEffect} from 'react';
+import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { UserModal } from '../UI';
+const LoginButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 5px;
+  height: 50px;
+  width: 100%;
+  cursor: pointer;
+  color: white;
+  text-decoration: none;
+  background:  black;
+  box-sizing: border-box;
+`;
+const LogoutButton = styled(LoginButton)`
+`;
 const UserCard = (props) => {
+  const user = useSelector(store => store.user.user);
   useEffect(()=>{
     document.body.style.overflowY = 'hidden';
     return ()=>{
@@ -9,7 +28,8 @@ const UserCard = (props) => {
   }, []);
   return (
     <UserModal>
-      UserCard
+      {!user && <LoginButton>登入</LoginButton>}
+      {user && <LogoutButton>登出</LogoutButton>}
     </UserModal>
   )
 }
