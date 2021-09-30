105db19463d1399ee792145c6e7bd4d3fb161c86
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-09-30 18:03:18 +0800

    Delete extra code which I forget to delete.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,17 +3,7 @@ import { useSelector } from "react-redux";
 import { MealsCard } from "../UI";
 import MealItem from "./MealItem";
 import DUMMY_MEALS from "../../DUMMY_MEALS";
-const processingFunc = (item) => {
-  let numOfCartItems = {};
-  for (let i = 0; i < item.length; i++) {
-    if(numOfCartItems[item[i].mealId] === undefined){
-      numOfCartItems[item[i].mealId] = item[i].amount;
-    }else{
-      numOfCartItems[item[i].mealId] += item[i].amount;
-    }
-  }
-  return numOfCartItems;
-}
+
 const AvailableMeals = (props) => {
   const MealsList = DUMMY_MEALS;
   const countedItems = useSelector(store => store.cart.countedItems);
