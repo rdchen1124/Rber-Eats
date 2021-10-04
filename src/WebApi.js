dd120ec7db9504c7bb1936ebcf0e3639beecdef3
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-10-04 17:07:50 +0800

    Create addUser and getUser functions for food_app to register user and login.

--- src/WebApi.js
+++ src/WebApi.js
@@ -1,6 +1,6 @@
 const base_url = 'http://localhost:3004';//在打API前，必須先啟動 json-server
 export const addOrder = (data) => {
-  return fetch(`${base_url}/orders`,{
+  return fetch(`${base_url}/orders`, {
     method:'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
@@ -9,5 +9,19 @@ export const addOrder = (data) => {
     +`&order=${JSON.stringify(data.order)}`
     +`&info=${JSON.stringify(data.info)}`
     +`&store=${JSON.stringify(data.store)}`
-  }).then(res => res.json())
+  }).then(res => res.json());
+}
+export const addUser = (data) => {
+  return fetch(`${base_url}/users`, {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body:`name=${data.name}`
+    +`password=${data.password}`
+    +`favorites=${JSON.stringify(data.favorites)}`
+  }).then(res => res.json());
+}
+export const getUser = (data) => {
+  return fetch(`${base_url}/users?name=${data.name}&password=${data.password}`).then(res => res.json());
 }
