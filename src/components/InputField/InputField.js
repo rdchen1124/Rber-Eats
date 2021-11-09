815a652455b317b513c3f2641074883fc194708c
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-11-09 16:17:36 +0800

    Add Inputfield component as reusable forminput for Login and Register.

---
+++ src/components/InputField/InputField.js
@@ -0,0 +1,24 @@
+import { forwardRef, Fragment } from "react";
+import styled from "styled-components";
+const InputLabel = styled.label`
+  color: grey;
+  margin-bottom: 5px;
+`
+const Input = styled.input`
+  height: 40px;
+  font-size: 18px;
+  padding: 5px 10px;
+`
+const InputField = forwardRef(({ label, name, placeholder, type }, ref) => (
+  <Fragment>
+    { label && <InputLabel htmlFor={name}>{label}</InputLabel> }
+    <Input
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      ref={ref}
+      id={name}
+    />
+  </Fragment>
+));
+export default InputField;
