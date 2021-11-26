1877c6c9860f2da60c631528b6f5795823d9039b
LAPTOP-5DDU3O4T\rdchen <rdchen1124@gmail.com>
2021-11-26 15:01:49 +0800

    Modify css style of Store page.

--- src/components/Root/StoreRoot.js
+++ src/components/Root/StoreRoot.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 const StoreRoot = styled.div`
   width: 100%;
   margin: 400px auto 0;
-  padding: 10px 0;
+  padding: 50px 0;
   background: rgba(255, 255, 255, 0.5);
 `
 export default StoreRoot;
